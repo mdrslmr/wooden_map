6f7b4cb04d239ab8fffb24baf5e0c28b6256127a
Michael Dressel <michael.dressel@kloenplatz.de>
2025-11-09 17:48:04 +0100

    Fix names and some refactoring

--- wooden_map.hs
+++ wooden_map.hs
@@ -13,31 +13,28 @@ genericPiece = [ V3 0 0 0, V3 0 0 1, V3 0 0 2, V3 0 0 3, V3 0 1 2 ]
 type Shape = [V3 Int]
 
 
-allSolutionsSmart :: [Shape]
-allSolutionsSmart = [[V3 1 1 1,V3 1 2 1,V3 1 3 1,V3 1 4 1,V3 1 3 2],[V3 1 1 5,V3 1 1 4,V3 1 1 3,V3 1 1 2,V3 1 2 3],[V3 4 2 2,V3 3 2 2,V3 2 2 2,V3 1 2 2,V3 2 2 3],[V3 4 2 4,V3 3 2 4,V3 2 2 4,V3 1 2 4,V3 2 2 5],[V3 1 5 5,V3 1 4 5,V3 1 3 5,V3 1 2 5,V3 1 3 4],[V3 2 3 1,V3 2 3 2,V3 2 3 3,V3 2 3 4,V3 1 3 3],[V3 1 4 2,V3 2 4 2,V3 3 4 2,V3 4 4 2,V3 3 3 2],[V3 1 5 1,V3 1 5 2,V3 1 5 3,V3 1 5 4,V3 1 4 3],[V3 4 4 4,V3 3 4 4,V3 2 4 4,V3 1 4 4,V3 2 4 5],[V3 5 1 1,V3 4 1 1,V3 3 1 1,V3 2 1 1,V3 3 1 2],[V3 2 1 2,V3 2 1 3,V3 2 1 4,V3 2 1 5,V3 3 1 4],[V3 2 2 1,V3 3 2 1,V3 4 2 1,V3 5 2 1,V3 4 3 1],[V3 3 1 5,V3 3 2 5,V3 3 3 5,V3 3 4 5,V3 2 3 5],[V3 5 4 1,V3 4 4 1,V3 3 4 1,V3 2 4 1,V3 3 3 1],[V3 2 5 5,V3 2 5 4,V3 2 5 3,V3 2 5 2,V3 2 4 3],[V3 2 5 1,V3 3 5 1,V3 4 5 1,V3 5 5 1,V3 4 5 2],[V3 3 1 3,V3 3 2 3,V3 3 3 3,V3 3 4 3,V3 3 3 4],[V3 3 5 2,V3 3 5 3,V3 3 5 4,V3 3 5 5,V3 4 5 4],[V3 4 1 2,V3 4 1 3,V3 4 1 4,V3 4 1 5,V3 5 1 4],[V3 4 5 3,V3 4 4 3,V3 4 3 3,V3 4 2 3,V3 4 3 2],[V3 4 5 5,V3 4 4 5,V3 4 3 5,V3 4 2 5,V3 4 3 4],[V3 5 1 2,V3 5 2 2,V3 5 3 2,V3 5 4 2,V3 5 3 1],[V3 5 4 3,V3 5 3 3,V3 5 2 3,V3 5 1 3,V3 5 2 4],[V3 5 1 5,V3 5 2 5,V3 5 3 5,V3 5 4 5,V3 5 3 4],[V3 5 5 2,V3 5 5 3,V3 5 5 4,V3 5 5 5,V3 5 4 4]]
+firstSolutionSmart :: [Shape]
+firstSolutionSmart = [[V3 1 1 1,V3 1 2 1,V3 1 3 1,V3 1 4 1,V3 1 3 2],[V3 1 1 5,V3 1 1 4,V3 1 1 3,V3 1 1 2,V3 1 2 3],[V3 4 2 2,V3 3 2 2,V3 2 2 2,V3 1 2 2,V3 2 2 3],[V3 4 2 4,V3 3 2 4,V3 2 2 4,V3 1 2 4,V3 2 2 5],[V3 1 5 5,V3 1 4 5,V3 1 3 5,V3 1 2 5,V3 1 3 4],[V3 2 3 1,V3 2 3 2,V3 2 3 3,V3 2 3 4,V3 1 3 3],[V3 1 4 2,V3 2 4 2,V3 3 4 2,V3 4 4 2,V3 3 3 2],[V3 1 5 1,V3 1 5 2,V3 1 5 3,V3 1 5 4,V3 1 4 3],[V3 4 4 4,V3 3 4 4,V3 2 4 4,V3 1 4 4,V3 2 4 5],[V3 5 1 1,V3 4 1 1,V3 3 1 1,V3 2 1 1,V3 3 1 2],[V3 2 1 2,V3 2 1 3,V3 2 1 4,V3 2 1 5,V3 3 1 4],[V3 2 2 1,V3 3 2 1,V3 4 2 1,V3 5 2 1,V3 4 3 1],[V3 3 1 5,V3 3 2 5,V3 3 3 5,V3 3 4 5,V3 2 3 5],[V3 5 4 1,V3 4 4 1,V3 3 4 1,V3 2 4 1,V3 3 3 1],[V3 2 5 5,V3 2 5 4,V3 2 5 3,V3 2 5 2,V3 2 4 3],[V3 2 5 1,V3 3 5 1,V3 4 5 1,V3 5 5 1,V3 4 5 2],[V3 3 1 3,V3 3 2 3,V3 3 3 3,V3 3 4 3,V3 3 3 4],[V3 3 5 2,V3 3 5 3,V3 3 5 4,V3 3 5 5,V3 4 5 4],[V3 4 1 2,V3 4 1 3,V3 4 1 4,V3 4 1 5,V3 5 1 4],[V3 4 5 3,V3 4 4 3,V3 4 3 3,V3 4 2 3,V3 4 3 2],[V3 4 5 5,V3 4 4 5,V3 4 3 5,V3 4 2 5,V3 4 3 4],[V3 5 1 2,V3 5 2 2,V3 5 3 2,V3 5 4 2,V3 5 3 1],[V3 5 4 3,V3 5 3 3,V3 5 2 3,V3 5 1 3,V3 5 2 4],[V3 5 1 5,V3 5 2 5,V3 5 3 5,V3 5 4 5,V3 5 3 4],[V3 5 5 2,V3 5 5 3,V3 5 5 4,V3 5 5 5,V3 5 4 4]]
 
 
 {-
- - Assigning symbols to the peaces parts:
+ - Assigning symbols to the pieces parts:
  -
  -     E
  - A B C D
  -
 -}
 
-data Symbol = O | A | B | C | D | E
+data Symbol = A | B | C | D | E
     deriving (Show, Eq, Ord)
 
 type Map = [(V3 Int, Symbol)]
 
-initMap :: Map
-initMap = []
+cube :: [Shape] -> Map
+cube = foldl piece []
 
-cube :: [Shape] -> Map -> Map
-cube xs m = foldl (flip peace) m xs
-
-peace :: Shape -> Map -> Map
-peace vs m = zip vs [A,B,C,D,E] ++ m
+piece :: Map -> Shape -> Map
+piece m vs = zip vs [A,B,C,D,E] ++ m
 
 formatMap :: Map -> String
 formatMap m = formatLines 1 (sortOn myord m)
@@ -56,15 +53,14 @@ formatLines _ [] = "\n"
 
 main :: IO ()
 main = do
-  let s = allSolutionsSmart
-  if length s == 25 
+  if length firstSolutionSmart == 25 
   then
         do 
-            putStrLn "The symbols are related to the peace like this: \n"
+            putStrLn "The symbols are related to the piece like this: \n"
             putStrLn "    E  "
             putStrLn "A B C D"
             putStrLn "\n"
-            putStrLn $ formatMap $ cube s initMap
+            putStrLn $ formatMap $ cube firstSolutionSmart
   else
         print "Wrong size for layer display"
     
